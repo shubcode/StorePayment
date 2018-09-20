7053c3e96edeeed1fb0cb1db7dab1934d8187ea3
shubcode <37629657+shubcode@users.noreply.github.com>
2018-09-19 23:41:49 -0500

    Create Database.java

---
+++ Database.java
@@ -0,0 +1,14 @@
+package Portfolio;
+
+// This class will handle information relating to the SQL Database which stores the information
+
+public class Database
+{
+    // constructor for class Database
+    public Database()
+    {
+
+    }
+
+
+}
