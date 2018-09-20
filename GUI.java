6c312a8a6a9f941a7e7c265578c49d263e8826db
shubcode <37629657+shubcode@users.noreply.github.com>
2018-09-20 00:55:27 -0500

    Update GUI.java

--- GUI.java
+++ GUI.java
@@ -1,11 +1,13 @@
 package Portfolio;
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 // This class handles the visual interface (GUI) of the program
 // The class will take in information and store it within an SQL Database
 
 
-public class GUI extends JFrame
+public class GUI extends JFrame implements ActionListener
 {
     // variables
 
@@ -14,6 +16,15 @@ public class GUI extends JFrame
     private int HEIGHT = 600;
 
     private JPanel homePanel;
+
+    private JButton addPayment;
+    private JButton removePayment;
+    private JButton closeApp;
+
+    private String paymentName;
+    private int paymentAmount;
+    private int confirmationNumber;
+    //private int paymentDate;
 
 
 
@@ -26,14 +37,71 @@ public class GUI extends JFrame
 
 
 
-    // Function to init
+    // Function to init / create GUI
     private void initProgramWindow()
     {
+        // creation of JFrame
         homeWindow = new JFrame("Payment Storage");
         homeWindow.setSize(WIDTH, HEIGHT);
         homeWindow.setResizable(false);
         homeWindow.setVisible(true);
         homeWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        homeWindow.setLocation(400,200);
+
+        // add panel to JFrame
+        homePanel = new JPanel();
+        homeWindow.add(homePanel);
+
+        // add a button
+        addPayment = new JButton("Add Payment");
+        addPayment.addActionListener(this);
+
+        removePayment = new JButton("Remove Payment");
+        removePayment.addActionListener(this);
+
+        closeApp = new JButton("Close App");
+        closeApp.addActionListener(this);
+
+        homePanel.add(addPayment);
+        homePanel.add(removePayment);
+        homePanel.add(closeApp);
+
+
+
+    }
+
+    public void actionPerformed(ActionEvent e)
+    {
+        String action = e.getActionCommand();
+
+        if ( action.equals("Add Payment"))
+        {
+            dataEntry();
+        }
+
+        else if (action.equals("Remove Payment"))
+        {
+
+
+        }
+
+        else if (action.equals("Close App"))
+        {
+
+            System.exit(0);
+        }
+
+    }
+
+
+
+
+    // Function that handles Data Entry
+    private void dataEntry()
+    {
+        // TO DO Add additional payment information
+        paymentName = JOptionPane.showInputDialog(null, "Enter Payment Name  ");
+
 
 
     }
