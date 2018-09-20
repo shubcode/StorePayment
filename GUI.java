b2935b4595f8eaff54df9535f065c5963e656cb6
shubcode <37629657+shubcode@users.noreply.github.com>
2018-09-20 00:05:49 -0500

    Update GUI.java

--- GUI.java
+++ GUI.java
@@ -1,12 +1,42 @@
 package Portfolio;
 import javax.swing.*;
 
-public class Main extends JFrame
-{
+// This class handles the visual interface (GUI) of the program
+// The class will take in information and store it within an SQL Database
 
-    public static void main(String[] args)
+
+public class GUI extends JFrame
+{
+    // variables
+
+    private JFrame homeWindow;
+    private int WIDTH = 800;
+    private int HEIGHT = 600;
+
+    private JPanel homePanel;
+
+
+
+    // constructor
+    public GUI()
     {
+        initProgramWindow();
+
+    }
+
+
+
+    // Function to init
+    private void initProgramWindow()
+    {
+        homeWindow = new JFrame("Payment Storage");
+        homeWindow.setSize(WIDTH, HEIGHT);
+        homeWindow.setResizable(false);
+        homeWindow.setVisible(true);
+        homeWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
 
     }
+
+
 }
